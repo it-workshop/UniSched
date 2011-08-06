ea99325fa791d7b96b376b26aa5f35182e8e86dd
Alexander <13alexac@gmail.com>
2011-08-06 23:39:36 +0400

    Fixed errors in human.{cc,h}.

--- human.cc
+++ human.cc
@@ -1,10 +1,9 @@
 #include <human.h>
 
-Human::Human(std::string name, std::string surname, Active_Time active)
+Human::Human(std::string name, std::string surname)
 {
     this->name = name;
     this->surname = surname;
-    this->active.push_back(active);
 }
 
 Human::~Human(void)
@@ -24,21 +23,5 @@ std::string Human::get_SurName(void)
 void Human::add_Group(Group *newbie)
 {
     groups.push_back(newbie);
-}
-
-void Human::add_Active(Active_Time active)
-{
-    this->active.push_back(active);
-}
-
-bool Human::is_Active(unsigned short int date)
-{
-    for (std::vector<Active_Time>::iterator it = active.begin(); it != active.end(); it++)
-    {
-        if ((*it).get_Begin() <= date && (*it).get_End() >= date)
-	    return true;
-    }
-
-    return false;
 }
 
--- human.h
+++ human.h
@@ -5,25 +5,21 @@
     #include <vector>
 
     #include <group.h>
-    #include <active_time.h>
 
     class Human {
     private:
         std::string name;
 	std::string surname;
-	std::vector<Active_Time> active;
         std::vector<class Group*> groups;
 
     public:
-        Human(std::string, std::string, Active_Time);
+        Human(std::string name, std::string surname);
 	~Human(void);
 
 	std::string get_Name(void);
 	std::string get_SurName(void);
 
         void add_Group(Group*);
-	void add_Active(Active_Time);
-	bool is_Active(unsigned short int);
     };
 
 #endif /* _HUMAN_H_ */
