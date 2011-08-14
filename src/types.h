bf0b83c11fcd4ee4888dfc9bc4629d69566dc930
Alexander <13alexac@gmail.com>
2011-08-14 20:11:07 +0400

    data storage class restyled.

---
+++ src/types.h
@@ -0,0 +1,16 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef unsigned long long id_type;
+
+typedef struct Group_Content_ Group_Content;
+
+class Person;
+class Event;
+class Group;
+class Calendar;
+class UserInterface;
+class Command;
+
+#endif /* _TYPES_H_ */
+
