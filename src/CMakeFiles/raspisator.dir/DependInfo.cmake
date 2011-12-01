48fa15bb07a19a95c5279ba7d7c465415288985f
WirelessWizard <wwwizz@gmail.com>
2011-12-01 18:53:30 +0400

    comment

---
+++ src/CMakeFiles/raspisator.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/calendar.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/calendar.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_add.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_add.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_all.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_all.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_clone.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_clone.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_delete.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_delete.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_dequeue.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_dequeue.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_enqueue.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_enqueue.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_exclude.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_exclude.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_exit.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_exit.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_generate.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_generate.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_help.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_help.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_include.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_include.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_link.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_link.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_merge.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_merge.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_print.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_print.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/command_select.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/command_select.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/commands.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/commands.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/data_storage.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/data_storage.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/event.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/event.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/event_template.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/event_template.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/file_storage.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/file_storage.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/group.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/group.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/main.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/main.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/person.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/person.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/queue.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/queue.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/table_data_storage.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/table_data_storage.cpp.o"
+  "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/userinterface.cpp" "/Users/arsh/Documents/workspace/issl_2011/UniSched/src/CMakeFiles/raspisator.dir/userinterface.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ src/CMakeFiles/raspisator.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/raspisator.dir/calendar.cpp.o"
+  "CMakeFiles/raspisator.dir/command_add.cpp.o"
+  "CMakeFiles/raspisator.dir/command_all.cpp.o"
+  "CMakeFiles/raspisator.dir/command_clone.cpp.o"
+  "CMakeFiles/raspisator.dir/command_delete.cpp.o"
+  "CMakeFiles/raspisator.dir/command_dequeue.cpp.o"
+  "CMakeFiles/raspisator.dir/command_enqueue.cpp.o"
+  "CMakeFiles/raspisator.dir/command_exclude.cpp.o"
+  "CMakeFiles/raspisator.dir/command_exit.cpp.o"
+  "CMakeFiles/raspisator.dir/command_generate.cpp.o"
+  "CMakeFiles/raspisator.dir/command_help.cpp.o"
+  "CMakeFiles/raspisator.dir/command_include.cpp.o"
+  "CMakeFiles/raspisator.dir/command_link.cpp.o"
+  "CMakeFiles/raspisator.dir/command_merge.cpp.o"
+  "CMakeFiles/raspisator.dir/command_print.cpp.o"
+  "CMakeFiles/raspisator.dir/commands.cpp.o"
+  "CMakeFiles/raspisator.dir/command_select.cpp.o"
+  "CMakeFiles/raspisator.dir/data_storage.cpp.o"
+  "CMakeFiles/raspisator.dir/event.cpp.o"
+  "CMakeFiles/raspisator.dir/event_template.cpp.o"
+  "CMakeFiles/raspisator.dir/file_storage.cpp.o"
+  "CMakeFiles/raspisator.dir/group.cpp.o"
+  "CMakeFiles/raspisator.dir/main.cpp.o"
+  "CMakeFiles/raspisator.dir/person.cpp.o"
+  "CMakeFiles/raspisator.dir/queue.cpp.o"
+  "CMakeFiles/raspisator.dir/table_data_storage.cpp.o"
+  "CMakeFiles/raspisator.dir/userinterface.cpp.o"
+  "raspisator.pdb"
+  "raspisator"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/raspisator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
