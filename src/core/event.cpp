44e4a7bed00c7c6e36caeefea5c683945c5482c9
Alexander Derbenew <13alexac@gmail.com>
2012-02-16 02:35:50 +0300

    Core::Event has renewed.

--- src/core/event.cpp
+++ src/core/event.cpp
@@ -1,85 +1,31 @@
 #include <event.h>
-#include <group.h>
 
 using namespace Core;
 
-void Event::save()
-{
-    AbstractGroup::save();
-
-    set_field("begin", begin_);
-    set_field("duration", duration_);
-}
-
-void Event::load()
-{
-    AbstractGroup::load();
-
-    begin_ = get_field_time("begin");
-    duration_ = get_field_time("duration");
-}
-
-const std::string Event::read() const
-{
-    std::stringstream stream(std::stringstream::out);
-
-    stream << "Start time:\t " << begin_ << std::endl
-           << "Duration:\t " << duration_ << std::endl
-           << "End time:\t " << begin_ + duration_ << std::endl
-           << AbstractGroup::read();
-
-    return stream.str();
-}
-
-const time_t Event::read_time(const std::string name) const throw (std::bad_cast)
-{
-    if (name == "begin")
-        { return begin_; }
-
-    if (name == "duration")
-        { return duration_; }
-
-    if (name == "end")
-        { return begin_ + duration_; }
-
-    return AbstractGroup::read_time(name);
-}
-
-void Event::update(const std::string name, const time_t value) throw(std::bad_cast)
+const Field& Event::read(const std::string& name) const throw (std::bad_cast)
 {
     if (name == "begin")
     {
-        begin_ = value;
-        set_field("begin", begin_);
-        return;
+        return begin_;
     }
-
     if (name == "duration")
     {
-        duration_ = value;
-        set_field("duration", duration_);
-        return;
+        return duration_;
     }
-
-    if (name == "end")
-    {
-        duration_ = value - begin_;
-        set_field("duration", duration_);
-        return;
-    }
-
-    return AbstractGroup::update(name, value);
+    return AbstractGroup::read(name);
 }
 
-void Event::update(UI::UsersObject * object, const bool linked) throw (std::bad_cast)
+void Event::update(const Field& field) throw (std::bad_cast)
 {
-    try
+    if (field.name() == "begin")
     {
-        AbstractGroup::update(object, linked);
+        begin_ = dynamic_cast<const FieldTime&>(field);
+        return;
     }
-    catch (std::bad_cast)
+    if (field.name() == "duration")
     {
-        (dynamic_cast<Group *>(object))->update(this, linked);
+        duration_ = dynamic_cast<const FieldTime&>(field);
     }
+    AbstractGroup::update(field);
 }
 
--- src/include/event.h
+++ src/include/event.h
@@ -6,29 +6,20 @@ namespace Core {
 
 class Event: public AbstractGroup {
 private:
-    time_t begin_;
-    time_t duration_;
+    FieldTime begin_;
+    FieldTime duration_;
 
 protected:
-    virtual void save();
-    virtual void load();
-
 public:
-    Event (const int id, Manager& manager):
-            AbstractGroup(id, manager) {}
-    Event (const int id, Manager& manager,
-        const std::string name, const time_t begin, const time_t duration):
-            AbstractGroup(id, manager, name), begin_(begin),
-            duration_(duration) {}
+    Event (const int id, Manager& manager) throw (std::bad_cast):
+            AbstractGroup(id, manager), begin_("begin"), duration_("duration")
+    {
+        begin_ = dynamic_cast<const FieldTime&>(pull("begin"));
+        duration_ = dynamic_cast<const FieldTime&>(pull("duration"));
+    }
 
-    const time_t begin() const { return begin_; }
-    const time_t duration() const { return duration_; }
-    const time_t end() const { return begin_ + duration_; }
-
-    virtual const std::string read() const;
-    virtual const time_t read_time(const std::string name) const throw (std::bad_cast);
-    virtual void update(const std::string name, const time_t value) throw (std::bad_cast);
-    virtual void update(UI::UsersObject * object, const bool linked) throw (std::bad_cast);
+    virtual const Field& read(const std::string& name) const throw (std::bad_cast);
+    virtual void update(const Field& field) throw (std::bad_cast);
 };
 
 };
