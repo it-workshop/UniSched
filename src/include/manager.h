836907e25f3bb98ce39092ebbdb24a1ee2ea7703
Alexander Derbenew <13alexac@gmail.com>
2012-02-16 12:22:18 +0300

    Manager has renewed. fix #25.

--- src/include/manager.h
+++ src/include/manager.h
@@ -2,7 +2,7 @@
 
 #include <string>
 #include <vector>
-#include <typeinfo>
+#include <map>
 
 #include <manageableobject.h>
 
@@ -19,7 +19,6 @@ namespace Core {
 class Manager {
 friend class ManageableObject;
 private:
-protected:
     std::vector<ManageableObject *> objects_;
                         /**< Model objects. Each object must be saved here for
                          * correct work of storage.
@@ -28,175 +27,12 @@ protected:
                          * new_id() mehods, you can redefine this methods but do
                          * not use this field in other code.
                          */
-    
-    virtual const int
-    get_field_int(const int id, const std::string field) const 
-            throw (std::bad_cast) = 0;
-                        /**< @brief Return integer value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @return requested value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * does not exists or keeped in another type.
-                         */
-
-    virtual const std::string
-    get_field_string(const int id, const std::string field) const
-            throw (std::bad_cast) = 0;
-                        /**< @brief Return string value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @return requested value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * does not exists or keeped in another type.
-                         */
-
-    virtual const time_t
-    get_field_time(const int id, const std::string field) const
-            throw (std::bad_cast) = 0;
-                        /**< @brief Return time value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @return requested value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * does not exists or keeped in another type.
-                         */
-
-    virtual const std::string
-    get_field_enum(const int id, const std::string field) const
-            throw (std::bad_cast) = 0;
-                        /**< @brief Return enum value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @return requested value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * does not exists or keeped in another type.
-                         */
-
-    virtual ManageableObject *
-    get_field_object(const int id, const std::string field) const
-            throw (std::bad_cast) = 0;
-                        /**< @brief Return object value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @return requested value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * does not exists or keeped in another type.
-                         */
-
-    virtual const std::vector<ManageableObject *>
-    get_field_vector(const int id, const std::string field) const
-            throw (std::bad_cast) = 0;
-                        /**< @brief Return vector value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @return requested value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * does not exists or keeped in another type.
-                         */
-
-    virtual void 
-    set_field(const int id, const std::string field, const int value)
-            throw (std::bad_cast) = 0;
-                        /**< @brief Set integer value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @param [in] value New value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * keeped in another type.
-                         */
-
-    virtual void 
-    set_field(const int id, const std::string field, const std::string value)
-            throw (std::bad_cast) = 0;
-                        /**< @brief Set string value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @param [in] value New value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * keeped in another type.
-                         */
-
-    virtual void 
-    set_field(const int id, const std::string field, const time_t value)
-            throw (std::bad_cast) = 0;
-                        /**< @brief Set time value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @param [in] value New value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * keeped in another type.
-                         */
-    virtual void 
-    set_field(const int id, const std::string field, ManageableObject * value)
-            throw (std::bad_cast) = 0;
-                        /**< @brief Set object value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @param [in] value New value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * keeped in another type.
-                         */
-
-    virtual void 
-    set_field_enum(const int id, const std::string field,
-        const std::string value)
-            throw (std::bad_cast) = 0;
-                        /**< @brief Set enum value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @param [in] value New value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * keeped in another type.
-                         */
-    virtual void
-    set_field_vector(const int id, const std::string field,
-        const std::vector<ManageableObject *> vector)
-            throw (std::bad_cast) = 0;
-                        /**< @brief Set vector value of requested field.
-                         * @param [in] id Object identificator.
-                         * @param [in] field Field name.
-                         * @paraw [in] value New value.
-                         *
-                         * This method must be realized in inherited classes.
-                         * Throw an std::bad_cast exception if requested field
-                         * keeped in another type.
-                         */
-
-    virtual void set_object(ManageableObject * object);
-                        /**< @brief Set integer value of requested field.
-                         * @param [in] Object to save.
-                         *
-                         * You can redefine this method if you want to keep
-                         * objects in other order.
-                         *
-                         * Do not use this method anywhere, it used by create
-                         * method.
-                         */
-
-    virtual const int new_id();
+    int new_id_;
+    const int new_id()
+    {
+        objects_.reserve(new_id_ + 1);
+        return new_id_++;
+    }
                         /**< @brief Return new object identificator and reserve
                          * cell for it in the objects vector.
                          * @return New id.
@@ -207,82 +43,124 @@ protected:
                          * Do not use this method anywhere, it used by create
                          * method.
                          */
+
+    std::vector<Field *> parameters_;
+    std::map<const int, std::vector<Field *>> records_;
+
+    ManageableObject * object(ManageableObject * object)
+    {
+        return objects_[object->id()] = object;
+    }
+
+protected:
+    void push(const int id, const Field& field)
+    {
+        for (Field * record: records_[id])
+        {
+            if (record->name() == field.name())
+            {
+                switch (field.type())
+                {
+                    case Field::STRING:
+                        delete [] record;
+                        record = new FieldString(
+                                dynamic_cast<const FieldString&>(field));
+                        break;
+                    case Field::TIME:
+                        delete [] record;
+                        record = new FieldTime(
+                                dynamic_cast<const FieldTime&>(field));
+                        break;
+                    case Field::ENUM:
+                        delete [] record;
+                        record = new FieldEnum(
+                                dynamic_cast<const FieldEnum&>(field));
+                        break;
+                    case Field::VECTOR:
+                        delete [] record;
+                        record = new FieldVector(
+                                dynamic_cast<const FieldVector&>(field));
+                        break;
+                    case Field::LINK:
+                        dynamic_cast<FieldVector *>(record)->commit(
+                                dynamic_cast<const FieldLink&>(field));
+                        break;
+                }
+                return;
+            }
+        }
+        switch(field.type())
+        {
+            case Field::STRING:
+                records_[id].push_back(new FieldString(
+                        dynamic_cast<const FieldString&>(field)));
+                break;
+            case Field::TIME:
+                records_[id].push_back(new FieldTime(
+                        dynamic_cast<const FieldTime&>(field)));
+                break;
+            case Field::ENUM:
+                records_[id].push_back(new FieldEnum(
+                        dynamic_cast<const FieldEnum&>(field)));
+                break;
+            case Field::VECTOR:
+                records_[id].push_back(new FieldVector(
+                        dynamic_cast<const FieldVector&>(field)));
+                break;
+            case Field::LINK:
+                records_[id].push_back(new FieldVector(field.name()));
+                dynamic_cast<FieldVector *>(records_[id][0])->commit(dynamic_cast<const FieldLink&>(field));
+                break;
+        }
+    }
+
+    const Field& pull(const std::string& name) const throw (std::bad_cast)
+    {
+        for (const Field* parameter: parameters_)
+        {
+            if (parameter->name() == name)
+            {
+                return *parameter;
+            }
+        }
+        throw std::bad_cast();
+    }
+    
 public:
-    /**@struct Argument
-     * @brief an auxiliary structure to make search query and create objects.
-     *
-     * set to the field name of the field (empty string for searching in the all
-     * fields of the type), set type and the value.
-     *
-     * Use constructors instead of manual editing.
-     */
-    struct Argument {
-        const std::string field;
-                        /**< Name of requested field. */
+    Manager() throw ():
+        new_id_(0)
+    {}
 
-        const enum {
-            STRING,
-            INTEGER,
-            TIME,
-            ENUMERATION
-        } type;
-                        /**< Type of the field. */
-
-        const std::string string;
-                        /**< String or enum value. */
-
-        const int integer;
-                        /**< Integer value. */
-
-        const time_t time;
-                        /**< Time value. */
-        
-        Argument(const std::string field, const std::string value):
-                field(field), type(STRING), string(value), integer(0), time(0)
-                {}
-                        /**< @brief Constructor for string type.
-                         * @param [in] field Name of the field.
-                         * @param [in] value String value.
-                         */
-        Argument(const std::string field, const int value):
-                field(field), type(INTEGER), integer(value), time(0)
-                {}
-                        /**< @brief Constructor for integer type.
-                         * @param [in] field Name of the field.
-                         * @param [in] value Integer value.
-                         */
-        Argument(const std::string field, const time_t value):
-                field(field), type(TIME), integer(0), time(value)
-                {}
-                        /**< @brief Constructor for time type.
-                         * @param [in] field Name of the field.
-                         * @param [in] value Time value.
-                         */
-        Argument(const std::string field, const std::string value,
-            const int number):
-                field(field), type(ENUMERATION), string(value),
-                integer(number), time(0)
-                {}
-                        /**< @brief Constructor for enumeration type.
-                         * @param [in] field Name of the field.
-                         * @param [in] value Enumeration value in the string
-                         * representation.
-                         * @param [in] number Enumoration value in the integer
-                         * representation.
-                         */
- 
-    };
-
-    virtual void remove(ManageableObject * object);
+    void remove(ManageableObject * object)
                         /**< @brief Remove object from the storage.
                          * @param [in] object Object to delete.
                          *
                          * You can redefine this method if you want to store
                          * objects in the other order.
                          */
+    {
+        for (Field* record : records_[object->id()])
+        {
+            if (record->type() == Field::VECTOR)
+            {
+                for (ManageableObject* obj:
+                    (dynamic_cast<FieldVector *>(record))->vector())
+                {
+                    object->update(FieldLink(record->name(),
+                                             std::make_pair(object, false)));
+                }
+            }
+        }
+        for (auto it = objects_.begin(); it != objects_.end(); it++)
+        {
+            objects_.erase(it);
+            delete [] object;
+            return;
+        }
+    }
 
     template <class T>
-    ManageableObject * create(std::vector<const Argument *>& parameters)
+    ManageableObject * create(std::vector<const Field>& parameters)
                         /**< @brief Create an object of the T type
                          * @param [in] parameters new object`s data.
                          * @return pointer to the created object.
@@ -291,37 +169,13 @@ public:
                          * create new object.
                          */
     {
-        ManageableObject * object = new T(new_id(), *this);
-    
-        set_object(object);
-
-        for (auto it = parameters.begin(); it != parameters.end(); it++)
-        {
-            switch ((*it)->type)
-            {
-            case Argument::STRING:
-                set_field(object->id(), (*it)->field, (*it)->string);
-                break;
-
-            case Argument::INTEGER:
-                set_field(object->id(), (*it)->field, (*it)->integer);
-                break;
-
-            case Argument::TIME:
-                set_field(object->id(), (*it)->field, (*it)->time);
-                break;
-
-            case Argument::ENUMERATION:
-                set_field_enum(object->id(), (*it)->field, (*it)->string);
-                break;
-            }
-        }
-
-        return object;
+        int id = new_id();
+        records_.insert(std::make_pair(id, (parameters_ = parameters)));
+        return object(new T(new_id(), *this));
     }
 
-    virtual std::vector<ManageableObject *> *
-    search(std::vector<Argument *>& parameters) = 0;
+    std::vector<ManageableObject *>&
+    search(const std::vector<const Field *>& parameters) const
                         /**< @brief Search objects by some parameters.
                          * @param [in] parameters Search parameters.
                          * @return Vector of found objects.
@@ -331,9 +185,30 @@ public:
                          * by the any field of specified type. If value
                          * is empty that all objects will satisfied.
                          */
+    {
+        static std::vector<ManageableObject *> results;
 
-    virtual ManageableObject * object(const int id) const throw (std::bad_cast)
-            { return objects_[id]; }
+        results.clear();
+
+        for (const Field * parameter: parameters)
+        {
+            for (const std::pair<const int, std::vector<Field *>>& objects_records :
+                records_)
+            {
+                for (const Field *record: objects_records.second)
+                {
+                    if (parameter == record)
+                    {
+                        results.push_back(objects_[objects_records.first]);
+                    }
+                }
+            }
+        }
+
+        return results;
+    }
+
+    ManageableObject * object(const int id) const throw (std::bad_cast)
                         /**< @brief Return object by id.
                          * @param [in] id Object identificator.
                          * @return Requested object.
@@ -341,6 +216,9 @@ public:
                          * Use this method carefully. It can be moved to the
                          * protected or private on future.
                          */
+    {                    
+        return objects_[id];
+    }
 };
 
 };
