ee2c84c851d036c5d82ce1ea444fad43970cde4e
Igor Mosyagin <cramur@mail.ru>
2012-03-05 16:57:43 +0400

    CLI stub

---
+++ src/modules/ui/cli.cpp
@@ -0,0 +1,25 @@
+#include <abstractui.h>
+
+class CommandLineInterface: public Core::AbstractUI {
+
+public:
+    void init (const std::vector< std::string > &args);
+    int run();
+    CommandLineInterface();
+};
+
+void CommandLineInterface::init(const std::vector<std::string>& args)
+{
+    std::cout << "CommandLine Interface INIT" << std::endl;
+}
+
+CommandLineInterface::CommandLineInterface() : AbstractUI("CLI") {
+}
+
+int CommandLineInterface::run()
+{
+    std::cout << "CommandLine Interface RUN" << std::endl;
+    return 0;
+}
+
+CommandLineInterface _object;
