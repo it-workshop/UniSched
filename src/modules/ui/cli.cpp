075802f74a91bffad6adef8c65f9c177b27a9772
Igor Mosyagin <cramur@mail.ru>
2012-03-05 23:04:48 +0400

    it's kind of magic, maaaagic

--- src/modules/ui/cli.cpp
+++ src/modules/ui/cli.cpp
@@ -4,37 +4,73 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+class CommandLineInterface;
+typedef int (CommandLineInterface::*CLIMemFunc)(void);
+
 class CommandLineInterface: public Core::AbstractUI {
+
+private:
+    std::map<std::string, CLIMemFunc> commands;
+    bool done;
 
 public:
     void init (const std::vector< std::string > &args);
+    int usage();
+    int quit();
     int run();
     CommandLineInterface(std::vector<Module *> *modules, void *handle);
 };
 
-void CommandLineInterface::init(const std::vector<std::string>& args)
-{
-    std::cout << "CommandLine Interface INIT" << std::endl;
-}
 
 CommandLineInterface::CommandLineInterface(std::vector<Module *> *modules,
         void *handle):
     AbstractUI("CLI", modules, handle)
 {}
 
+void CommandLineInterface::init(const std::vector<std::string>& args)
+{
+    std::cout << "CommandLine Interface INIT" << std::endl;
+    done = false;
+    commands.insert(std::make_pair("quit", &CommandLineInterface::quit));
+    commands.insert(std::make_pair("usage", &CommandLineInterface::usage));
+    commands.insert(std::make_pair("help", &CommandLineInterface::usage));
+}
+
+int CommandLineInterface::quit() {
+    done = true;
+    return 0;
+}
+
+int CommandLineInterface::usage() {
+    std::cout << "Usage information on our little UniSched Sandbox Edition\n";
+    std::cout << "Not so much so far! :]\n";
+    std::cout << "\n";
+    std::cout << " quit -- quit\n";
+    std::cout << " help -- this text\n";
+    std::cout << " usage -- this text.\n";
+    std::cout << "\n";
+    std::cout << " See? Told you so!" << std::endl;
+    return 0;
+}
+
 int CommandLineInterface::run()
 {
     // Nasty nasty C-style
     char *buf = new char(255);
-    while ((buf = readline("\n >> ")) != NULL)  {
-        if (strcmp(buf, "quit") == 0) {
-            break;
+    while (!done) {
+        if ((buf = readline("\n >> ")) == NULL)  {
+            done = true;
         }
 
         std::cout << " << " << buf << std::endl;
 
         if(buf[0] != 0) {
             add_history(buf);
+        }
+
+        if (commands.find(buf) != commands.end()) {
+            // DON'T TOUCH THE LINE BELOW, MOTHEFUCKER
+            (this->*commands[buf])();
         }
 
     }
