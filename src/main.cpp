61855aba461af6780ed27f7701eb1e1c1da949c2
Alexander Derbenew <13alexac@gmail.com>
2012-03-05 16:47:37 +0300

    Old commented code is removed.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ bool
 load_modules (Core::AbstractUI **ui,
                std::vector<std::string>& args)
 {
-/*    *ui = nullptr;*/
     *ui = nullptr;
     std::string uiname;
     for (auto it = args.begin(); it != args.end(); it++)
@@ -20,30 +19,6 @@ load_modules (Core::AbstractUI **ui,
     }
     for (Module *module: modules())
     {
-/*        if (*ui && *ui)
-        {
-            break;
-        }
-        if (module->type() == Module::STORAGE)
-        {
-            if (*ui)
-            {
-                continue;
-            }
-            try
-            {
-                *ui = dynamic_cast<Core::AbstractUI *>(module);
-                module->init(args);
-            }
-            catch (std::bad_cast e)
-            {
-                std::cerr << "Warning: invalid ui module!" << e.what() <<
-		            std::endl;
-                *ui = nullptr;
-            }
-            continue;
-        }*/
-
         if (module->type() == Module::UI)
         {
             if (*ui || (!uiname.empty() && module->name() != uiname))
@@ -66,11 +41,6 @@ load_modules (Core::AbstractUI **ui,
     }
 
     bool error = false;
-/*    if (!*ui)
-    {
-        error = true;
-        std::cerr << "Error: ui module not found!" << std::endl;
-    }*//* storage module is not always needed. */
     if (!*ui)
     {
         error = true;
@@ -83,14 +53,13 @@ load_modules (Core::AbstractUI **ui,
 int
 main(int argc, char *argv[])
 {
-/*    Core::AbstractUI *ui = nullptr;*/
     Core::AbstractUI *ui = nullptr;
 
     std::vector<std::string> args;
     for (unsigned int i = 1; i < argc; i++)
         { args.push_back(std::string(argv[i])); }
 
-    if (load_modules(/*&ui,*/ &ui, args))
+    if (load_modules(&ui, args))
     {
         return -1;
     }
