69f63fd967456e182c2fb2b4dd2ae311258992f4
Alexander Derbenew <13alexac@gmail.com>
2012-03-08 23:38:45 +0300

    Minor fix in Core::Object::update method.

--- src/core/object.cpp
+++ src/core/object.cpp
@@ -5,14 +5,16 @@ using namespace Core;
 void Object::update(const std::string& name, const boost::any& value)
 {
     check_field(name, value);
-
     if (value.type() != typeid(std::pair<Object *,bool>))
     {
         fields_[name] = value;
         ui_.push(id(), name, value);
         return;
     }
-
+    if (fields_[name].empty())
+    {
+        fields_[name] = std::vector<Object *>();
+    }
     auto tmp_pair = boost::any_cast<std::pair<Object *,bool>>(value);
     auto tmp_vect = boost::any_cast<std::vector<Object *>>(fields_[name]);
     if (tmp_pair.second)
