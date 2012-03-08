600520b4f9ebc055411be6cd6b1a487301480603
Igor Mosyagin <cramur@mail.ru>
2012-03-09 01:35:20 +0400

    stub для new_person в CLI

--- src/modules/ui/cli.cpp
+++ src/modules/ui/cli.cpp
@@ -9,35 +9,38 @@ void CommandLineInterface::init(const std::vector<std::string>& args)
 {
     std::cout << "CommandLine Interface INIT" << std::endl;
     done = false;
-    NoArgsCommands.insert(std::make_pair("quit", &CommandLineInterface::quit));
-    NoArgsCommands.insert(std::make_pair("clear", &CommandLineInterface::clear));
-    NoArgsCommands.insert(std::make_pair("usage", &CommandLineInterface::usage));
-    NoArgsCommands.insert(std::make_pair("help", &CommandLineInterface::usage));
-    NoArgsCommands.insert(std::make_pair("history", &CommandLineInterface::history));
 
-    std::for_each(NoArgsCommands.begin(), 
-        NoArgsCommands.end(), 
-        [this] (std::pair<const std::string, CLIMemFunc>& p) 
+    Commands.insert(std::make_pair("quit", &CommandLineInterface::quit));
+    Commands.insert(std::make_pair("clear", &CommandLineInterface::clear));
+    Commands.insert(std::make_pair("usage", &CommandLineInterface::usage));
+    Commands.insert(std::make_pair("help", &CommandLineInterface::usage));
+    Commands.insert(std::make_pair("history", &CommandLineInterface::history));
+
+    Commands.insert(std::make_pair("new_person", &CommandLineInterface::new_person));
+    Commands.insert(std::make_pair("new_group", &CommandLineInterface::new_group));
+
+    std::for_each(Commands.begin(), 
+        Commands.end(), 
+        [this] (std::pair<const std::string, CLIMemCommand>& p) 
     { Completions.push_back(p.first); } );
-
 
     Reader.RegisterCompletions(Completions);
 }
 
-void CommandLineInterface::quit() {
+void CommandLineInterface::quit(const std::vector<std::string>& unused) {
     done = true;
 }
 
-void CommandLineInterface::clear() {
+void CommandLineInterface::clear(const std::vector<std::string>& unused) {
     Reader.ClearHistory();
 }
 
-void CommandLineInterface::history() {
-    std::cout << "Historrrrry listing not working yet :)" << std::endl;
+void CommandLineInterface::history(const std::vector<std::string>& unused) {
+    std::cout << "TODO: implement history listing" << std::endl;
     //reader.save_history(std::cout);
 }
 
-void CommandLineInterface::usage() {
+void CommandLineInterface::usage(const std::vector<std::string>& unused) {
     std::cout << "Usage information on our little UniSched Sandbox Edition\n"
         << "Not so much so far! :]\n"
         << "\n"
@@ -50,20 +53,30 @@ void CommandLineInterface::usage() {
         << " See? Told you so!" << std::endl;
 }
 
+void new_group(const std::vector<std::string>& fields) {
+    std::cout << "TODO: implement" << std::endl;
+}
+
+void new_person(const std::vector<std::string>& fields) {
+    std::cout << "TODO: implement" << std::endl;
+}
+
 
 int CommandLineInterface::run()
 {
     std::string input;
     const std::string prompt = "RASPISATOR-REX>> ";
     const std::string history_path = "/tmp/raspisator"; // NB should be customizable
+
     Reader.LoadHistory(history_path);
     do {
         input = Reader.GetLine(prompt, done);
         if(done) break;
         if(input.empty()) continue;
-        if (NoArgsCommands.find(input) != NoArgsCommands.end()) {
+        if (Commands.find(input) != Commands.end()) {
             // DON'T TOUCH THE LINE BELOW, MOTHERFUCKER
-            (this->*NoArgsCommands[input])();
+            std::vector<std::string> v; v.push_back("test");
+            (this->*Commands[input])(v);
             continue;
         }
         std::cout << "Unknown command: " << input << std::endl;
--- src/modules/ui/cli.hpp
+++ src/modules/ui/cli.hpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include <malloc.h>
+#include <vector>
+#include <string>
 
 #include <boost/format.hpp>
 
 #include <abstractui.h>
+#include <person.h>
+#include <group.h>
 
 #include "SReadline.h"
 
 
 class CommandLineInterface;
-typedef void (CommandLineInterface::*CLIMemFunc)(void);
+typedef void (CommandLineInterface::*CLIMemCommand)(const std::vector<std::string> &);
 
 class CommandLineInterface: public Core::AbstractUI {
 
 private:
-    std::map<std::string, CLIMemFunc> NoArgsCommands;
+    std::map<std::string, CLIMemCommand> Commands;
     bool done;
     swift::SReadline Reader;
     std::vector<std::string> Completions;
@@ -24,9 +28,12 @@ public:
     int run();
     CommandLineInterface(std::vector<Module *> *modules, void *handle);
 
-    void usage();
-    void quit();
-    void clear();
-    void history();
+    void usage(const std::vector<std::string>& unused);
+    void quit(const std::vector<std::string>& unused);
+    void clear(const std::vector<std::string>& unused);
+    void history(const std::vector<std::string>& unused);
+
+    void new_person(const std::vector<std::string>& fields);
+    void new_group(const std::vector<std::string>& fields);
 };
 
