b997a5de98f57d627f7b1bb042a4f37108303a45
Alexander Derbenew <13alexac@gmail.com>
2012-03-10 20:29:54 +0300

    sqlite storage is renewed.

--- src/modules/storage/sqlite.cpp
+++ src/modules/storage/sqlite.cpp
@@ -1,164 +1,40 @@
 #include <abstractui.h>
 
-class SQLiteStorage: public Storage::AbstractStorage {
+class SQLiteStorage: public Core::AbstractStorage {
 
-protected:
-    virtual const int
-    get_field_int(const int id, const std::string name) const
-            throw (std::bad_cast);
-
-    virtual const
-    std::string get_field_string(const int id, const std::string name) const
-            throw (std::bad_cast);
-
-    virtual const time_t
-    get_field_time(const int id, const std::string name) const
-            throw (std::bad_cast);
-
-    virtual const std::string
-    get_field_enum(const int id, const std::string name) const
-            throw (std::bad_cast);
-
-    virtual Storage::StorableObject *
-    get_field_object(const int id, const std::string name) const
-            throw (std::bad_cast);
-
-    virtual const std::vector<Storage::StorableObject *>
-    get_field_vector(const int id, const std::string name) const
-            throw (std::bad_cast);
-
-    virtual void
-    set_field(const int id, const std::string name, const int value)
-            throw (std::bad_cast);
-
-    virtual void
-    set_field(const int id, const std::string name, const std::string value)
-            throw (std::bad_cast);
-
-    virtual void
-    set_field(const int id, const std::string name, const time_t value)
-            throw (std::bad_cast);
-
-    virtual void
-    set_field_enum(const int id, const std::string name,
-        const std::string value)
-            throw (std::bad_cast);
-
-    virtual void
-    set_field(const int id, const std::string name,
-        Storage::StorableObject *value)
-            throw (std::bad_cast);
-
-    virtual void
-    set_field_vector(const int id, const std::string name,
-        const std::vector<Storage::StorableObject*> value)
-            throw (std::bad_cast);
 public:
-    virtual void init(const std::vector<std::string>& args);
+    SQLiteStorage(std::vector<Core::Module *>* modules, void *handle):
+            AbstractStorage("SQLITE", modules, handle)
+    {}
 
-    virtual std::vector<Storage::StorableObject*>*
-    search(std::vector<Storage::AbstractStorage::Argument*>& args);
+    virtual void init(const std::vector<std::string>& args);
+    virtual void push(const std::string& name, const boost::any& value);
+    virtual void load();
+    virtual void save();
 };
 
 void SQLiteStorage::init(const std::vector<std::string>& args)
 {
-    //TODO: empty method :(
 }
 
-const int
-SQLiteStorage::get_field_int(const int id, const std::string name) const
-        throw (std::bad_cast)
+void SQLiteStorage::push(const std::string& name, const boost::any& value)
 {
-    //TODO: empty method :(
 }
 
-const std::string
-SQLiteStorage::get_field_string(const int id, const std::string name) const
-        throw (std::bad_cast)
+void SQLiteStorage::load()
 {
-    //TODO: empty method :(
 }
 
-const time_t
-SQLiteStorage::get_field_time(const int id, const std::string name) const
-        throw (std::bad_cast)
+void SQLiteStorage::save()
 {
-    //TODO: empty method :(
 }
 
-const std::string
-SQLiteStorage::get_field_enum(const int id, const std::string name) const
-        throw (std::bad_cast)
+extern "C" {
+
+void init(std::vector<Core::Module *>* modules, void *handle)
 {
-    //TODO: empty method :(
+    new SQLiteStorage(modules, handle);
 }
 
-Storage::StorableObject *
-SQLiteStorage::get_field_object(const int id, const std::string name) const
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-const std::vector<Storage::StorableObject *>
-SQLiteStorage::get_field_vector(const int id, const std::string name) const
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-void
-SQLiteStorage::set_field(const int id, const std::string name, const int value)
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-void
-SQLiteStorage::set_field(const int id, const std::string name,
-    const std::string value)
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-void
-SQLiteStorage::set_field(const int id, const std::string name,
-    const time_t value)
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-void
-SQLiteStorage::set_field_enum(const int id, const std::string name,
-    const std::string value)
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-void
-SQLiteStorage::set_field(const int id, const std::string name,
-    Storage::StorableObject *value)
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-void
-SQLiteStorage::set_field_vector(const int id, const std::string name,
-    const std::vector<Storage::StorableObject*> value)
-        throw (std::bad_cast)
-{
-    //TODO: empty method :(
-}
-
-std::vector<Storage::StorableObject*>*
-SQLiteStorage::search(std::vector<Storage::AbstractStorage::Argument*>& args)
-{
-    //TODO: empty method :(
-}
-
-SQLiteStorage _object;
+};
 
