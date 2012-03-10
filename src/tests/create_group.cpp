c71746d5b4d95d02487ae4f042a21f391416c18d
Alexander Derbenew <13alexac@gmail.com>
2012-03-10 21:17:18 +0300

    Create group test added.

---
+++ src/tests/create_group.cpp
@@ -0,0 +1,22 @@
+#include <assert.h>
+
+#include <iostream>
+#include <typeinfo>
+
+#include <group.h>
+
+#define TESTINGUI_NAME "TESTINGUI_CREATE_GROUP"
+#include "testingui.h"
+
+
+int TestingUI::run()
+{
+    Core::Object *object = create<Core::Group>();
+
+    assert(object);
+
+    std::cout << typeid(object).name() << std::endl;
+
+    return 0;
+}
+
