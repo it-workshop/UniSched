bdf7aa16415cbbaad4b91532d5033487c0b1007d
Alexander Derbenew <13alexac@gmail.com>
2012-03-10 22:29:33 +0300

    Set defined field wrong test is added.

---
+++ src/tests/set_defined_field_wrong.cpp
@@ -0,0 +1,30 @@
+#include <assert.h>
+
+#include <iostream>
+#include <typeinfo>
+
+#include <person.h>
+
+#define TESTINGUI_NAME "TESTINGUI_SET_DEFINED_FIELD_WRONG"
+#include "testingui.h"
+
+
+int TestingUI::run()
+{
+    Core::Object *object = create<Core::Person>();
+
+    assert(object);
+
+    try
+    {
+        object->update("name", 0);
+    }
+    catch (boost::bad_any_cast& e)
+    {   
+        std::cout << e.what() << std::endl;
+        return 0;
+    }
+
+    return -1;
+}
+
