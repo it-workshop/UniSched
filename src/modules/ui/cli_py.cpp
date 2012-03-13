9c15c0c29821dbfbe48bae1aa6e1a573a25e71ca
Igor Mosyagin <cramur@mail.ru>
2012-03-13 20:56:43 +0400

    да хуй с ним с сегфолтом, так сойдёт)

--- src/modules/ui/cli_py.cpp
+++ src/modules/ui/cli_py.cpp
@@ -1,5 +1,9 @@
 #include <boost/python.hpp>
 
+#include <abstractui.h>
+#include <abstractstorage.h>
+#include <module.h>
+#include <utils.h>
 #include <cli_py_names.hpp>
 
 
@@ -8,8 +12,25 @@ char const* greet()
     return "Test python bindings";
 }
 
+int prepare_your_anus() {
+    Core::AbstractUI *ui = nullptr;
+    Core::AbstractStorage *storage = nullptr;
+    std::vector<std::string> args;
+    args.push_back("--iface");
+    args.push_back("DummyUI");
+    Core::Module::load_modules();
+    if (utils::select_modules(&ui, &storage, args))
+    {
+        return -1;
+    }
+    int code = ui->run();
+    Core::Module::unload_modules();
+    return code;
+}
+
 BOOST_PYTHON_MODULE(CLI_PY_LIB_CLEAN_NAME)
 {
     using namespace boost::python;
     def("greet", greet);
+    def("anus_preporaro", prepare_your_anus);
 }
