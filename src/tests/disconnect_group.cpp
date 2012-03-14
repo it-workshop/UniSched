0e67b9f7214e0d6078a5ddce750d322fd51f655f
Alexander Derbenew <13alexac@gmail.com>
2012-03-14 12:03:46 +0300

    Test "disconnect group" is added and passed.

---
+++ src/tests/disconnect_group.cpp
@@ -0,0 +1,31 @@
+#include <assert.h>
+
+#include <iostream>
+#include <typeinfo>
+
+#include <person.h>
+#include <group.h>
+
+#define TESTINGUI_NAME "TESTINGUI_DISCONNECT_GROUP"
+#include "testingui.h"
+
+
+int TestingUI::run()
+{
+    Core::Object *group1 = create<Core::Group>();
+    assert(group1);
+    Core::Object *group2 = create<Core::Group>();
+    assert(group2);
+
+    group1->connect(group2);
+    group1->disconnect(group2);
+
+    if (boost::any_cast<const std::vector<Core::Object *>&>(group1->read("children_groups")).size()
+        || boost::any_cast<const std::vector<Core::Object *>&>(group2->read("parent_groups")).size())
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
