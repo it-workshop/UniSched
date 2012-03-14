5e3baaddb04d202cd2e03642b9fff1da8e3062ee
Alexander Derbenew <13alexac@gmail.com>
2012-03-14 15:12:35 +0300

    Test "remove connected object" is added.

---
+++ src/tests/remove_connected_object.cpp
@@ -0,0 +1,48 @@
+#include <assert.h>
+
+#include <iostream>
+#include <typeinfo>
+
+#include <person.h>
+#include <group.h>
+
+#define TESTINGUI_NAME "TESTINGUI_REMOVE_CONNECTED_OBJECT"
+#include "testingui.h"
+
+
+int TestingUI::run()
+{
+    Core::Object *John = create<Core::Person>();
+    assert(John);
+    Core::Object *James = create<Core::Person>();
+    assert(James);
+    Core::Object *group1 = create<Core::Group>();
+    assert(group1);
+    Core::Object *group2 = create<Core::Group>();
+    assert(group2);
+
+    John->connect(group1);
+    John->connect(group2);
+    group1->connect(James);
+    group2->connect(James);
+    group2->connect(group1);
+
+    remove(group1);
+    remove(James);
+
+    if (boost::any_cast<std::vector<Core::Object *>>(John->read("groups")).size() != 1)
+    {
+        return -1;
+    }
+    if (boost::any_cast<std::vector<Core::Object *>>(group2->read("people")).size() != 1)
+    {
+        return -2;
+    }
+    if (boost::any_cast<std::vector<Core::Object *>>(group2->read("children_groups")).size())
+    {
+        return -3;
+    }
+
+    return 0;
+}
+
