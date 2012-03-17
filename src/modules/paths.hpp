0749416f85f48c17feee2cd2f45dcfb47a9a4ae2
Igor Mosyagin <cramur@mail.ru>
2012-03-17 17:47:59 +0400

    генерируемый файлам в репозитории не место

--- src/modules/paths.hpp
+++
@@ -1,6 +0,0 @@
-#ifdef WITH_YAML
-#define DATABASE_YAML "/home/wi/workplace/UniSched/fixtures/database.yaml"
-#endif
-#define GROUPS_CSV "/home/wi/workplace/UniSched/fixtures/groups.csv"
-#define PERSONS_CSV "/home/wi/workplace/UniSched/fixtures/persons.csv"
-#define EVENTS_CSV "/home/wi/workplace/UniSched/fixtures/events.csv"
