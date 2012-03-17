72daa5f3542200b78e94e4a9181273e874483e12
Igor Mosyagin <cramur@mail.ru>
2012-03-17 18:42:22 +0400

    генерируемые файлы нахнах

--- src/include/paths.hpp
+++
@@ -1,6 +0,0 @@
-#ifdef WITH_YAML
-#define DATABASE_YAML "/home/alex/code/UniSched/fixtures/database.yaml"
-#endif
-#define GROUPS_CSV "/home/alex/code/UniSched/fixtures/groups.csv"
-#define PERSONS_CSV "/home/alex/code/UniSched/fixtures/persons.csv"
-#define EVENTS_CSV "/home/alex/code/UniSched/fixtures/events.csv"
