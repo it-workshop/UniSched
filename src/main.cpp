7ec93b87da8607f8a06ed2aabc0b2102d89c4122
Alexander Derbenew <13alexac@gmail.com>
2012-03-29 22:34:08 +0400

    Core switched to std::w* output. locale settings upd.

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,23 @@ main(int argc, char *argv[])
     Core::AbstractUI *ui = nullptr;
     Core::AbstractStorage *storage = nullptr;
     std::vector<std::wstring> args;
+    
+    std::clog << "libc locale: " << std::setlocale(LC_ALL, "") << std::endl;
+    std::locale::global(std::locale(""));
+    std::clog << "stl C++ locale: " << std::locale().name() << std::endl;
+    std::cout.imbue(std::locale());
+    std::cerr.imbue(std::locale());
+    std::clog.imbue(std::locale());
+    std::cin.imbue(std::locale());
+    std::wcout.imbue(std::locale());
+    std::wcerr.imbue(std::locale());
+    std::wclog.imbue(std::locale());
+    std::wcin.imbue(std::locale());
+    std::ios::sync_with_stdio(false);
+    utils::init_iconv();
+
     for (unsigned int i = 1; i < argc; i++)
         { args.push_back(std::wstring(utils::iconv(argv[i]))); }
-    setlocale(LC_ALL, "");
-    utils::init_iconv();
     Core::Module::load_modules();
     if (utils::select_modules(&ui, &storage, args))
     {
