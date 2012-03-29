40b31bf1f94e13e39bb41e5f5fc33a800e2eda5d
Alexander Derbenew <13alexac@gmail.com>
2012-03-29 23:48:20 +0400

    DummyUI and ncurses modules are updated.

--- src/modules/ui/dummy.hpp
+++ src/modules/ui/dummy.hpp
@@ -12,7 +12,7 @@
 class DummyInterface: public Core::AbstractUI {
 
 public:
-    void init (const std::vector< std::string > &args);
+    void init (const std::vector< std::wstring > &args);
     int run();
     void test_person();
     void test_group();
--- src/modules/ui/ncurses/main.cpp
+++ src/modules/ui/ncurses/main.cpp
@@ -9,16 +9,16 @@ public:
 
     ncursesUI(std::vector<Core::Module *>* modules, void *handle);
 
-    virtual void init(const std::vector<std::string>& args);
+    virtual void init(const std::vector<std::wstring>& args);
     virtual int run();
 };
 
 ncursesUI::ncursesUI(std::vector<Core::Module *>* modules, void *handle):
-        AbstractUI("ncurses", modules, handle)
+        AbstractUI(L"ncurses", modules, handle)
 {
 }
 
-void ncursesUI::init(const std::vector<std::string>& args)
+void ncursesUI::init(const std::vector<std::wstring>& args)
 {
 }
 
