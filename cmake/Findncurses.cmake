b737576523fcd327b9013fe02df6ef6a847d3bb3
Alexander Derbenew <13alexac@gmail.com>
2012-03-30 17:00:53 +0400

    Revert "Findncurses.cmake fix."
    This reverts commit 91339fc3cb626dda2cab2bb2046c8568a59658db.
    
    Conflicts:
    
    	src/core/utils.cpp

--- cmake/Findncurses.cmake
+++ cmake/Findncurses.cmake
@@ -1,5 +1,5 @@
 find_path(NCURSES_INCLUDE_DIR ncurses.h)
-find_library(NCURSES_LIBRARY NAMES ncurses)
+find_library(NCURSES_LIBRARY NAMES ncurses3)
 
 if (NCURSES_INCLUDE_DIR AND NCURSES_LIBRARY)
     set(NCURSES_FOUND TRUE)
