77da23c59774785e00dce748e966950eb206e649
Alexander Derbenew <13alexac@gmail.com>
2012-03-30 19:57:32 +0400

    Tests are fixed.

--- src/tests/tester.cpp
+++ src/tests/tester.cpp
@@ -1,69 +1,41 @@
-#include <abstractui.h>
-#include <abstractui.h>
-#include <module.h>
-
 #include <iostream>
 
-bool
-select_modules (Core::AbstractUI **ui,
-               std::vector<std::string>& args)
-{
-    *ui = nullptr;
-    std::string uiname;
-    for (auto it = args.begin(); it != args.end(); it++)
-    {
-        if (*it == "--iface")
-        {
-            uiname = *++it;
-        }
-    }
-    for (Core::Module *module: *Core::Module::modules())
-    {
-        if (module->type() == Core::Module::UI)
-        {
-            if (*ui || (!uiname.empty() && module->name() != uiname))
-            {
-                continue;
-            }
-            try
-            {
-                *ui = dynamic_cast<Core::AbstractUI *>(module);
-                module->init(args);
-            }
-            catch (std::bad_cast e)
-            {
-                std::cerr << "Warning: invalid ui module!" << e.what() <<
-                    std::endl;
-                *ui = nullptr;
-            }
-            continue;
-        }
-    }
+#include <locale.h>
 
-    bool error = false;
-    if (!*ui)
-    {
-        error = true;
-        std::cerr << "Error: ui module not found!" << std::endl;
-    }
+#include <abstractui.h>
+#include <abstractstorage.h>
+#include <module.h>
+#include <utils.h>
 
-    return error;
-}
 
 int
 main(int argc, char *argv[])
 {
     Core::AbstractUI *ui = nullptr;
+    Core::AbstractStorage *storage = nullptr;
     std::vector<std::string> args;
+    setenv("UNISCHED_MODULES_PATH", ".", 1);
     for (unsigned int i = 1; i < argc; i++)
         { args.push_back(std::string(argv[i])); }
+    setlocale(LC_ALL, "");
+    utils::init_iconv();
     Core::Module::load_modules();
-    if (select_modules(&ui, args))
+    if (utils::select_modules(&ui, &storage, args))
     {
         return -1;
     }
+    if (storage)
+    {
+        storage->set_UI(ui);
+        storage->connect();
+    }
     int code = ui->run();
+    if (storage)
+    {
+        storage->disconnect();
+    }
     Core::Module::unload_modules();
+    utils::deinit_iconv();
     return code;
 }
 
