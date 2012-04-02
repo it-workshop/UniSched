1fac2f67be2727db684086047e6a38815d4172da
Alexander Derbenew <13alexac@gmail.com>
2012-04-02 17:49:02 +0400

    create(type) lua function is added.

--- src/modules/ui/luaui.cpp
+++ src/modules/ui/luaui.cpp
@@ -1,4 +1,7 @@
 #include <abstractui.h>
+#include <person.h>
+#include <group.h>
+#include <event.h>
 
 extern "C" {
 
@@ -9,9 +12,11 @@ extern "C" {
 };
 
 class luaUI : public Core::AbstractUI {
+friend int luaUI_create(lua_State *state);
 private:
     std::string script_;
     lua_State *vm_;
+    std::vector<Core::Object *> objects_;
 protected:
 public:
     luaUI(std::vector<Core::Module *>* modules, void *handle);
@@ -42,11 +47,56 @@ void luaUI::init(const std::vector<std::string>& args)
     }
 }
 
+static luaUI *self;
+
+int luaUI_create(lua_State *state)
+{
+    if (lua_gettop(state) != 1)
+    {
+        lua_pushstring(state, "Incorrect argument!");
+        lua_error(state);
+        return 0;
+    }
+    std::string type = lua_tostring(state, 1);
+    if ("person" == type)
+    {
+        self->objects_.push_back(self->create<Core::Person>());
+    }
+    else if ("group" == type)
+    {
+        self->objects_.push_back(self->create<Core::Group>());
+    }
+    else if ("event" == type)
+    {
+        self->objects_.push_back(self->create<Core::Event>());
+    }
+    else
+    {
+        lua_pushstring(state, "Unknown type!");
+        lua_error(state);
+        return 0;
+    }
+
+    lua_createtable(state, 0, 0);
+    lua_pushstring(state, type.c_str());
+    lua_setfield(state, -2, "__type");
+    lua_pushnumber(state, self->objects_.size() - 1);
+    lua_setfield(state, -2, "__id");
+    lua_getglobal(state, "object");
+    lua_setmetatable(state, -2);
+    return 1;
+}
+
 int luaUI::run()
 {
+    self = this;
     vm_ = lua_open();
-
+    lua_createtable(vm_, 0, 0);
+    lua_setglobal(vm_, "object");
+    
     luaL_openlibs(vm_);
+    lua_register(vm_, "create", luaUI_create);
+
     luaL_loadfile(vm_, script_.c_str());
     lua_pcall(vm_, 0, LUA_MULTRET, 0);
     
