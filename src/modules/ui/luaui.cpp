e2b956ad773c156c0e25a6fb1569ae776388fa08
Alexander Derbenew <13alexac@gmail.com>
2012-04-03 16:04:32 +0400

    lua function create upd. api is completely declarated.

--- src/modules/ui/luaui.cpp
+++ src/modules/ui/luaui.cpp
@@ -14,11 +14,17 @@ extern "C" {
 };
 
 class luaUI : public Core::AbstractUI {
+friend int luaUI___object___index(lua_State *state);
+friend int luaUI___object___newindex(lua_State *state);
+friend int luaUI___object___eq(lua_State *state);
 friend int luaUI_create(lua_State *state);
 friend int luaUI_search(lua_State *state);
 friend int luaUI_remove(lua_State *state);
+friend int luaUI_object_type(lua_State *state);
 friend int luaUI_object_read(lua_State *state);
 friend int luaUI_object_update(lua_State *state);
+friend int luaUI_object_connect(lua_State *state);
+friend int luaUI_object_disconnect(lua_State *state);
 private:
     std::string script_;
     lua_State *vm_;
@@ -55,241 +61,150 @@ void luaUI::init(const std::vector<std::string>& args)
 
 static luaUI *self;
 
-int luaUI_create(lua_State *state)
+int luaUI_object_type(lua_State *state)
 {
-    if (lua_gettop(state) != 1)
-    {
-        lua_pushstring(state, "Incorrect argument!");
-        lua_error(state);
-        return 0;
-    }
-    std::string type = lua_tostring(state, 1);
-    if ("person" == type)
-    {
-        self->objects_.push_back(self->create<Core::Person>());
-    }
-    else if ("group" == type)
-    {
-        self->objects_.push_back(self->create<Core::Group>());
-    }
-    else if ("event" == type)
-    {
-        self->objects_.push_back(self->create<Core::Event>());
-    }
-    else
-    {
-        lua_pushstring(state, "Unknown type!");
-        lua_error(state);
-        return 0;
-    }
-
-    lua_createtable(state, 0, 0);
-    lua_pushstring(state, type.c_str());
-    lua_setfield(state, -2, "__type");
-    lua_pushnumber(state, self->objects_.size() - 1);
-    lua_setfield(state, -2, "__id");
-    lua_getglobal(state, "__object");
-    lua_setmetatable(state, -2);
-    return 1;
-}
-
-int luaUI_search(lua_State *state)
-{
-    if (lua_gettop(state) != 1 || lua_istable(state, 1))
-    {
-        lua_pushstring(state, "Incorrect argument!");
-        lua_error(state);
-        return 0;
-    }
-
-    lua_pushnil(state);
-    std::map<std::string, boost::any> args;
-    while(lua_next(state, -2))
-    {
-        if (!lua_isstring(state, -2) || lua_isstring(state, -1))    //TODO: add seacrh by time.
-        {
-            std::cerr << "lua UI: search argument is not string!" << std::endl;
-            continue;
-        }
-        args[lua_tostring(state, -2)] = lua_tostring(state, -1);
-        lua_pop(state, 1);
-    }
-
-    auto ret = self->search(args);
-
-    int i = 1;
-    lua_createtable(state, 0, 0);
-    for (auto found : ret)
-    {
-        lua_createtable(state, 0, 0);
-        switch (found->type())
-        {
-        case Core::PERSON:
-            lua_pushstring(state, "person");
-            break;
-        case Core::GROUP:
-            lua_pushstring(state, "group");
-            break;
-        case Core::EVENT:
-            lua_pushstring(state, "event");
-            break;
-        default:
-            lua_pushstring(state, "unknown");
-            break;
-        }
-        lua_setfield(state, -2, "__type");
-        lua_pushnumber(state, self->objects_.size());
-        self->objects_.push_back(found);
-        lua_setfield(state, -2, "__id");
-        lua_getglobal(state, "__object");
-        lua_setmetatable(state, -2);
-        lua_setfield(state, -2, boost::str(boost::format("%u") % i++).c_str());
-    }
-
-    return 1;
-}
-
-int luaUI_remove(lua_State *state)
-{
-    if (lua_gettop(state) != 1 || !lua_istable(state, 1))
-    {
-        lua_pushstring(state, "Invalid argument!");
-        lua_error(state);
-        return 0;
-    }
-    lua_getfield(state, 1, "__id");
-    if (!lua_isnumber(state, -1))
-    {
-        lua_pushstring(state, "This is not object!");
-        lua_error(state);
-        return 0;
-    }
-    self->remove(self->objects_.at(lua_tonumber(state, -1)));
     return 0;
 }
 
 int luaUI_object_read(lua_State *state)
 {
-    if (lua_gettop(state) != 2 || !lua_istable(state, 1) || !lua_isstring(state, 2))
-    {
-        lua_pushstring(state, "Invalid argument!");
-        lua_error(state);
-        return 0;
-    }
-    lua_getfield(state, 1, "__id");
-    if (!lua_isnumber(state, -1))
-    {
-        lua_pushstring(state, "This is not object!");
-        lua_error(state);
-        return 0;
-    }
-    auto ret = self->objects_.at(lua_tonumber(state, -2))->read(lua_tostring(state, 2));
-    if (ret.type() == typeid(const std::string))
-    {
-        lua_pushstring(state, boost::any_cast<const std::string&>(ret).c_str());
-    }
-    else if (ret.type() == typeid(const time_t))
-    {
-        lua_pushnumber(state, boost::any_cast<const time_t>(ret));
-    }
-    else if (ret.type() == typeid(const std::vector<Core::Object *>))
-    {
-        lua_createtable(state, 0, 0);
-        int i = 0;
-        for (auto object : boost::any_cast<const std::vector<Core::Object *>>(ret))
-        {
-            lua_createtable(state, 0, 0);
-            switch (object->type())
-            {
-            case Core::PERSON:
-                lua_pushstring(state, "person");
-                break;
-            case Core::GROUP:
-                lua_pushstring(state, "group");
-                break;
-            case Core::EVENT:
-                lua_pushstring(state, "event");
-                break;
-            default:
-                lua_pushstring(state, "unknown");
-                break;
-            }
-            lua_setfield(state, -2, "__type");
-            lua_pushnumber(state, self->objects_.size());
-            self->objects_.push_back(object);
-            lua_setfield(state, -2, "__id");
-            lua_getglobal(state, "__object");
-            lua_setmetatable(state, -2);
-            lua_setfield(state, -2, boost::str(boost::format("%u") % i++).c_str());
-        }
-    }
-    else
-    {
-        lua_pushstring(state, "Unknown type!");
-        lua_error(state);
-        return 0;
-    }
-    return 1;
-}
-
-static int luaUI_object___index(lua_State *state)
-{
-    return luaUI_object_read(state);
+    return 0;
 }
 
 int luaUI_object_update(lua_State *state)
 {
-    if (lua_gettop(state) != 3 || !lua_istable(state, 1) || !lua_isstring(state, 2) || (!lua_isnumber(state, 3) and !lua_isstring(state, 3)))
-    {
-        lua_pushstring(state, "Invalid arguments!");
-        lua_error(state);
-        return 0;
-    }
-    lua_getfield(state, 1, "__id");
-    if (!lua_isnumber(state, -1))
-    {
-        lua_pushstring(state, "This is not table!");
-        lua_error(state);
-        return 0;
-    }
-    std::string name = lua_tostring(state, 2);
-    boost::any value;
-    if (lua_isnumber(state, 3))
-    {
-        value = (time_t) lua_tonumber(state, 3);
-    }
-    else
-    {
-        value = std::string(lua_tostring(state, 3));
-    }
-    self->objects_.at(lua_tonumber(state, -1))->update(name, value);
     return 0;
 }
 
-static int luaUI_object___newindex(lua_State *state)
+int luaUI_object_connect(lua_State *state)
 {
-    return luaUI_object_update(state);
+    return 0;
+}
+
+int luaUI_object_disconnect(lua_State *state)
+{
+    return 0;
+}
+
+int luaUI___object___index(lua_State *state)
+{
+    return 0;
+}
+
+int luaUI___object___newindex(lua_State *state)
+{
+    return 0;
+}
+
+int luaUI___object___eq(lua_State *state)
+{
+    return 0;
+}
+
+int luaUI_create(lua_State *state)
+{
+    /* stack:
+     *  1 - type, must be string which value is 'person', 'group' or 'event'.
+     */
+    if (lua_gettop(state) != 1 || !lua_isstring(state, 1))
+    {
+        lua_pushstring(state, "Invalid argument!");
+        lua_error(state);
+        // long jump in lua_error().
+    }
+    Core::Object *object = nullptr;
+    if (lua_tostring(state, 1) == std::string("person"))
+    {
+        object = self->create<Core::Person>();
+    }
+    else if (lua_tostring(state, 1) == std::string("group"))
+    {
+        object = self->create<Core::Group>();
+    }
+    else if (lua_tostring(state, 1) == std::string("event"))
+    {
+        object = self->create<Core::Event>();
+    }
+    else
+    {
+        lua_pushstring(state, "Invalid type!");
+        lua_error(state);
+        // long jump
+    }
+    int id = self->objects_.size();
+    self->objects_.push_back(object);               // Stack:
+    lua_createtable(state, 0, 0);                           // #-1: object
+
+    lua_pushnumber(state, id);                              // #-2: object, #-1: id
+    lua_pushcclosure(state, luaUI_object_type, 1);          // #-2: object, #-1: [id]type()
+    lua_setfield(state, -2, "type");                        // #-1: object
+    
+    lua_pushnumber(state, id);                              // #-2: object, #-1: id
+    lua_pushcclosure(state, luaUI_object_read, 1);          // #-2: object, #-1: [id]read()
+    lua_setfield(state, -2, "read");                        // #-1: object
+
+    lua_pushnumber(state, id);                              // #-2: object, #-1: id
+    lua_pushcclosure(state, luaUI_object_update, 1);        // #-2: object, #-1: [id]update()
+    lua_setfield(state, -2, "update");                      // #-1: object
+    
+    lua_pushnumber(state, id);                              // #-2: object, #-1: id
+    lua_pushcclosure(state, luaUI_object_connect, 1);       // #-2: object, #-1: [id]connect()
+    lua_setfield(state, -2, "connect");                     // #-1: object
+
+    lua_pushnumber(state, id);                              // #-2: object, #-1: id
+    lua_pushcclosure(state, luaUI_object_disconnect, 1);    // #-2: object, #-1: [id]disconnect()
+    lua_setfield(state, -2, "disconnect");                  // #-1: object
+
+    lua_pushnumber(state, id);                              // #-2: object, #-1: id
+    lua_setfield(state, -2, "__varid");                     // #-1: object
+
+    lua_getglobal(state, "__object");                       // #-2: object, #-1: __object
+    lua_setmetatable(state, -2);                            // #-1: object
+
+    return 1;                                               // return object
+}
+
+int luaUI_search(lua_State *state)
+{
+    return 0;
+}
+
+int luaUI_remove(lua_State *state)
+{
+    return 0;
 }
 
 int luaUI::run()
 {
     self = this;
     vm_ = lua_open();
-    lua_createtable(vm_, 0, 0);
-    lua_pushcfunction(vm_, luaUI_object_read);
-    lua_setfield(vm_, -2, "read");
-    lua_pushcfunction(vm_, luaUI_object___index);
-    lua_setfield(vm_, -2, "__index");
-    lua_pushcfunction(vm_, luaUI_object_update);
-    lua_setfield(vm_, -2, "update");
-    lua_pushcfunction(vm_, luaUI_object___newindex);
-    lua_setfield(vm_, -2, "__newindex");
-    lua_setglobal(vm_, "__object");
-    
+   
     luaL_openlibs(vm_);
+
+    lua_createtable(vm_, 0, 0);
+                    //  __object = {
+    lua_pushcfunction(vm_, luaUI___object___index);
+    lua_setfield(vm_, -2, "__index");
+                    //      __index = function (self, index) ... end,
+    lua_pushcfunction(vm_, luaUI___object___newindex);
+    lua_setfield(vm_, -2, "__newindex");
+                    //      __newindex = function (self, index, value) ... end,
+    lua_pushcfunction(vm_, luaUI___object___eq);
+    lua_setfield(vm_, -2, "__eq");
+                    //      __eq = function (self, with) ... end
+    lua_setglobal(vm_, "__object");
+                    // }
+
+    lua_createtable(vm_, 0, 0);
+    lua_setglobal(vm_, "cache");
+                    // cache = {}
+
     lua_register(vm_, "create", luaUI_create);
-    lua_register(vm_, "search", luaUI_create);
-    lua_register(vm_, "remove", luaUI_create);
+                    // function create (type) ... end
+    lua_register(vm_, "search", luaUI_search);
+                    //function search (args) ... end
+    lua_register(vm_, "remove", luaUI_remove);
+                    //function remove(object) ... end
 
     luaL_loadfile(vm_, script_.c_str());
     lua_pcall(vm_, 0, LUA_MULTRET, 0);
