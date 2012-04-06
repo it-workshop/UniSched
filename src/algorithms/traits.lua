02da0172ae3de3c6e0af8e922ea335059fa57be7
Alexander Derbenew <13alexac@gmail.com>
2012-04-06 20:56:36 +0400

    traits optimization is added.

--- src/algorithms/traits.lua
+++ src/algorithms/traits.lua
@@ -20,7 +20,7 @@ local parse_traits_list = function(s)
     end
 end
 
-local parse_traits = function(s)
+algorithms.parse_traits = function(s)
     local allow, deny = string.match(s, '(.+)\(.+)')
     local traits = {}
     traits.allow, traits.deny = parse_traits_list(allow), parse_traits_list(deny)
@@ -38,7 +38,58 @@ local make_traits_list = function(traits)
     end
 end
 
-local make_traits = function(traits)
+algorithms.make_traits = function(traits)
     return string.format('%s|%s', make_traits_list(traits.allow), make_traits_list(traits.deny))
+end
+
+algorithms.traits_optimize = function(traits)
+    local comp = function(lhs, rhs)
+        return lhs.from < rhs.from
+    end
+    sort(traits.allow, comp)
+    local i = 1
+    while i < #(traits.allow) do
+        if traits.allow[i].to >= traits.allow[i + 1].from + 1 then
+            traits.allow[i].to = math.max(traits.allow[i].to, traits.allow[i + 1].to)
+            table.remove(traits.allow, i + 1)
+        end
+    end
+    sort(traits.deny, comp)
+    i = 1
+    while i < #(traits.deny) do
+        if traits.deny[i].to >= traits.deny[i].from + 1 then
+            traits.deny[i].to = math.max(traits.deny[i].to, traits.deny[i + 1].to)
+            table.remove(traits.deny, i + 1)
+        end
+    end
+    local j = 1
+    i = 1
+    while i <= #(traits.allow) or j <= #(traits.deny) do
+        if traits.allow[i].from < traits.deny[j].from then
+            if traits.allow[i].to >= traits.deny[j].from then
+                if traits.allow[i].to <= traits.deny[j].to then
+                    traits.allow[i].to = traits.deny[j].from - 1
+                else
+                    table.insert(traits.allow, i + 1, {from = traits.deny[j].to + 1, to = traits.allow[j].to})
+                    i, j = i + 1, j + 1
+                end
+            end
+            i = i + 1
+        else
+            if traits.allow[i].from <= traits.deny[j].to then
+                if traits.allow[i].to > traits.deny[j].to then
+                    traits.allow[i].from = traits.deny[j].to + 1
+                    j = j + 1
+                else
+                    table.remove(traits.allow[i])
+                end
+            else
+                j = j + 1
+            end
+        end
+    end
+    if #(traits.allow) > 0 then
+        table.remove(traits.deny)
+    end
 end
 
