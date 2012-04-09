7ea3d32c3eb17c085e04228eebdf7337d7827684
Alexander Derbenew <13alexac@gmail.com>
2012-04-10 00:29:45 +0400

    algorithms.traits

--- src/algorithms/traits.lua
+++ src/algorithms/traits.lua
@@ -1,48 +1,43 @@
 #!/usr/bin/env lua
 
-local parse_time = function(s)
-    local t = {}
-    t.year, t.month, t.day, t.hour, t.min, t.sec = string.match(s, '(%d%d%d%d)%-(%d%d)%-(%d%d) (%d%d):(%d%d):(%d%d)')
-    return os.time(t)
-end
-
-local parse_trait = function(s)
-    local from, to = string.match(s, '%((%d%d%d%d%-%d%d%-%d%d %d%d:%d%d:%d%d)|(%d%d%d%d%-%d%d%-%d%d %d%d:%d%d:%d%d)%)')
-    local trait = {}
-    trait.from, to = parse_time(from), parse_time(to)
-    return trait
-end
-
-local parse_traits_list = function(s)
-    local traits = {}
-    for trait in string.gmatch(s, '%(%d%d%d%d%-%d%d-%d%d %d%d:%d%d:%d%d|%d%d%d%d%-%d%d%-%d%d %d%d:%d%d:%d%d%)') do
-        table.insert(traits, parse_trait(trait))
-    end
-end
-
 algorithms.parse_traits = function(s)
+    local parse_time = function(s)
+        local t = {}
+        t.year, t.month, t.day, t.hour, t.min, t.sec = string.match(s, '(%d%d%d%d)%-(%d%d)%-(%d%d) (%d%d):(%d%d):(%d%d)')
+        return os.time(t)
+    end
+    local parse_trait = function(s)
+        local from, to = string.match(s, '%((%d%d%d%d%-%d%d%-%d%d %d%d:%d%d:%d%d)|(%d%d%d%d%-%d%d%-%d%d %d%d:%d%d:%d%d)%)')
+        local trait = {}
+        trait.from, to = parse_time(from), parse_time(to)
+        return trait
+    end
+    local parse_traits_list = function(s)
+        local traits = {}
+        for trait in string.gmatch(s, '%(%d%d%d%d%-%d%d-%d%d %d%d:%d%d:%d%d|%d%d%d%d%-%d%d%-%d%d %d%d:%d%d:%d%d%)') do
+            table.insert(traits, parse_trait(trait))
+        end
+    end
     local allow, deny = string.match(s, '(.+)\(.+)')
     local traits = {}
     traits.allow, traits.deny = parse_traits_list(allow), parse_traits_list(deny)
     return traits
 end
 
-local make_trait = function(trait)
-    return string.format('(%s|%s)', os.date('%H %T', trait.from), os.date('%H %T', trait.to))
-end
-
-local make_traits_list = function(traits)
-    local s = ''
-    for i, trait in pairs(traits) do
-        s = s..make_trait(trait)
-    end
-end
-
 algorithms.make_traits = function(traits)
+    local make_trait = function(trait)
+        return string.format('(%s|%s)', os.date('%H %T', trait.from), os.date('%H %T', trait.to))
+    end
+    local make_traits_list = function(traits)
+        local s = ''
+        for i, trait in pairs(traits) do
+            s = s..make_trait(trait)
+        end
+    end
     return string.format('%s|%s', make_traits_list(traits.allow), make_traits_list(traits.deny))
 end
 
-algorithms.traits_optimize = function(traits)
+algorithms.traits_optimize = function(traits, duration)
     local comp = function(lhs, rhs)
         return lhs.from < rhs.from
     end
@@ -90,6 +85,13 @@ algorithms.traits_optimize = function(traits)
             else
                 j = j + 1
             end
+        end
+    end
+    while i <= #(traits.allow) do
+        if traits.allow[i].to - traits.allow[i].from < duration then
+            table.remove(traits, i)
+        else
+            i = i + 1
         end
     end
     if #(traits.allow) > 0 then
