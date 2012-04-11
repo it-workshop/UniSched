29a8311a41396b8984c59625a22ddda1487a6b2d
Alexander Derbenew <13alexac@gmail.com>
2012-04-11 22:00:23 +0400

    algorithms.schedule is added. It is dummy schedule algorithm.

--- src/algorithms/conflicts.lua
+++ src/algorithms/conflicts.lua
@@ -1,6 +1,6 @@
 #!/usr/bin/env lua
 
-algorithms.found_conflicts = function (event)
+function algorithms.found_conflicts(event)
     local events = {}
     local function get_people(obj)
         local people = {}
@@ -36,4 +36,49 @@ algorithms.found_conflicts = function (event)
             end
         end
     end
+    sort (events, function (lhs, rhs) return lhs.start < rhs.start end)
+    return events
 end
+
+function algorithms.schedule (events)
+    local function schedule(event)
+        local function busy(event)
+            if event.start then return { from = event.start, to = event.start + event.duration } end
+            return { from = event.probe, to = event.probe + event.duration }
+        end
+        if event.start or event.probe then return 0 end
+        local traits = algorithms.parse_traits(event.traits)
+        local conflicts = algorithms.find_conflicts(event)
+        for key, conflict in pairs(conflicts) do
+            if conflict.start or conflict.probe then
+                table.insert(traits.deny, {busy(conflict)})
+            end
+        end
+        
+        for key, trait in pairs(traits) do
+            event.probe = trait.from
+            for key, conflict in pairs(conflict) do
+                if not schedule(conflict) then
+                    event.probe = nil
+                    break
+                end
+            end
+            if event.probe then
+                event.start, event.probe = event.probe, nil
+                return 1
+            end
+        end
+        return nil
+    end
+
+    for key, event in pairs(events) do
+        local traits = algorithms.parse_traits(event.traits)
+        algorithms.optimize_traits(traits)
+        event.traits = algorithms.make_traits(traits)
+    end
+
+    for key, event in pairs(events) do
+        schedule(event)
+    end
+end
+
