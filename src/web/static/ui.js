ccb11124fab775479baa5b050ca1d2e017240213
Alexander Derbenew <13alexac@gmail.com>
2012-08-14 01:53:19 +0400

    jquery list widget. $(...).list(), $(...).list(append, id, text), $(...).list(change, function(event, target) {})

--- src/web/static/ui.js
+++ src/web/static/ui.js
@@ -1,34 +1,12 @@
-/*
-function list(object, change) {
-    object.addClass('ui-widget-content').addClass('ui-corner-bottom').addClass('ui-corner-top').
-        addClass('ui-state-active').children().
-            addClass('ui-state-default').hover(
-                function(event){if($(event.target).hasClass('ui-state-default'))
-                    $(event.target).removeClass('ui-state-default').addClass('ui-state-hover')},
-                function(event){if($(event.target).hasClass('ui-state-hover'))
-                    $(event.target).removeClass('ui-state-hover').addClass('ui-state-default');}).
-            click(function(event){if($(event.target).hasClass('ui-state-hover')) {
-                object.children().removeClass('ui-state-active').addClass('ui-state-default');
-                $(event.target).removeClass('ui-state-hover').removeClass('ui-state-default').
-                    addClass('ui-state-active');
-                    change(event)}});
-}
 
-function add_item(object, text, id) {
-    $('<div>').text(text).attr('id', id).addClass('ui-
-}
-*/
 $(document).ready(function() {
     $('.tabs').tabs();
     $('.accordion').accordion();
-    list($('.list'))
+    $('.list').list();
 
     $.getJSON('/api/group/', function (data) {
         $.each(data, function (i, group) {
-            $('<div>').addClass('item').
-                addClass('ui-state-default').text(group.name).appendTo($('#groups-list')).hover(
-                    function(event) {$(event.target).removeClass('ui-state-default').addClass('ui-state-hover');},
-                    function(event) {$(event.target).removeClass('ui-state-hover').addClass('ui-state-default');});
+            $('#groups-list').list('append', group.id, group.name);
         });
     });
 
