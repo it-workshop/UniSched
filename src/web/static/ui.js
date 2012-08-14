4e02b7c376d31ad16d96557a21432b5cac6905cd
Loredan <loredan@loredan.(none)>
2012-08-15 00:09:56 +0400

    People tab has been made just as Groups tab. Added buttons and search line above list of people.

--- src/web/static/ui.js
+++ src/web/static/ui.js
@@ -1,4 +1,5 @@
 var groups = [];
+var people = [];
 
 $(document).ready(function() {
     $('.tabs').tabs();
@@ -30,6 +31,34 @@ $(document).ready(function() {
         $.each(data, function (i, group) {
             $('#groups-list').list('append', group.id, group.name);
             groups[group.id] = group;
+        });
+    });
+    
+    $('#people-list').list({change: function(event, target) {
+        var person = people[$(target).attr('id')];
+        $('#person-info').empty();
+        $.each(person, function (key, value) {
+            if ( key != 'id' && key != 'type' ) {
+                if (typeof(value) != 'object') {
+                    $('<tr id=field-' + 
+                        key + '><th>' + key +
+                        '</th><td><input type=text class="ui-corner-all ui-widget-content" value="'+
+                        value +'"></td></tr>').
+                        appendTo($('#person-info'));
+                } else {
+                    $('<tr id=field-' +
+                        key + '><th>' + key +
+                        '</th><td>' + value.length + '</td></tr>').
+                        appendTo($('#person-info'));
+                }
+            }
+        });
+    }});
+
+    $.getJSON('/api/person/', function (data) {
+        $.each(data, function (i, person) {
+            $('#people-list').list('append', person.id, person.name);
+            people[person.id] = person;
         });
     });
 });
