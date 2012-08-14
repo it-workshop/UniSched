8e752d124ae81b1666a0a6c7d94b6bc2c163cd75
Alexander Derbenew <13alexac@gmail.com>
2012-08-14 13:52:05 +0400

    group info table.

--- src/web/static/ui.js
+++ src/web/static/ui.js
@@ -1,14 +1,36 @@
+var groups = [];
 
 $(document).ready(function() {
     $('.tabs').tabs();
     $('.accordion').accordion();
     $('.list').list();
 
+    $('#groups-list').list({change: function(event, target) {
+        var group = groups[$(target).attr('id')];
+        $('#group-info').empty();
+        $.each(group, function (key, value) {
+            if ( key != 'id' && key != 'type' ) {
+                if (typeof(value) != 'object') {
+                    $('<tr id=field-' + 
+                        key + '><th>' + key +
+                        '</th><td><input type=text class="ui-corner-all ui-widget-content" value="'+
+                        value +'"></td></tr>').
+                        appendTo($('#group-info'));
+                } else {
+                    $('<tr id=field-' +
+                        key + '><th>' + key +
+                        '</th><td>' + value.length + '</td></tr>').
+                        appendTo($('#group-info'));
+                }
+            }
+        });
+    }});
+
     $.getJSON('/api/group/', function (data) {
         $.each(data, function (i, group) {
             $('#groups-list').list('append', group.id, group.name);
+            groups[group.id] = group;
         });
     });
-
 });
 
