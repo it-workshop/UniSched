53bd19e9c662b4b763d88bd6bb2968b0abc6684a
Loredan <loredan@loredan.(none)>
2012-08-19 20:25:15 +0400

    Faster loading

--- src/web/static/list.js
+++ src/web/static/list.js
@@ -62,7 +62,37 @@ $.widget( "ui.list" , {
 
     append: function (id, text) {
         $('<li id=' + id + '>' + text + '</li>').appendTo(this.element);
-        this.refresh();
+        this.element.children('[id=' + id + ']').
+            removeClass('ui-state-active').
+            removeClass('ui-state-hover').
+            addClass('ui-state-default').
+            hover(
+            function(event) {
+                if ($(event.target).hasClass('ui-state-default')) {
+                    $(event.target).
+                        removeClass('ui-state-default').
+                        addClass('ui-state-hover');
+                }
+            },
+            function(event) {
+                if ($(event.target).hasClass('ui-state-hover')) {
+                    $(event.target).
+                        removeClass('ui-state-hover').
+                        addClass('ui-state-default');
+                }
+            }).click(function (event) {
+                if ($(event.target).hasClass('ui-state-hover')) {
+                    _self.element.children().
+                        removeClass('ui-state-active').
+                        addClass('ui-state-default');
+                    $(event.target).
+                        removeClass('ui-state-hover').
+                        removeClass('ui-state-default').
+                        addClass('ui-state-active');
+                    _self._trigger("change", event, [event.target]);
+                }
+            });
+//        this.refresh();
     }
 });
 
